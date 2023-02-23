99f0217a16a8c22c532e11c22dde511b149a900d
Memetova Evelina <57720564+milinia@users.noreply.github.com>
2023-02-23 22:19:55 +0300

    Initial Commit

---
+++ BookMyTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookMyTable
+//
+//  Created by Evelina on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
